1b36540f08a96ff60510ca80d3878ed043909e21
SokolAK <sokolak87@gmail.com>
2024-03-02 01:03:46 +0100

    🍃 Init Spring Boot project

---
+++ src/test/java/pl/sokolak/teamtelly/TeamTellyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sokolak.teamtelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTellyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

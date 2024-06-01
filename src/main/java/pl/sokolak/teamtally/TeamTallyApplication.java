97f50ae794ce00a4781dafa90b61139acb46511c
SokolAK <sokolak87@gmail.com>
2024-06-02 00:03:07 +0200

    Add PWA

--- src/main/java/pl/sokolak/teamtally/TeamTallyApplication.java
+++ src/main/java/pl/sokolak/teamtally/TeamTallyApplication.java
@@ -2,12 +2,14 @@ package pl.sokolak.teamtally;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Theme("dashboard-blue")
+@PWA(name = "TeamTally", shortName = "TeamTally")
 public class TeamTallyApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
